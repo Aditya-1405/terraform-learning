0d0e00edae23f81ebc65a1d72b1b618ad5df4ed5
Aditya Damar <adityadamar2000@gmail.com>
2025-01-02 11:22:50 +0530

    removed hardcoded values

--- vault-int/main.tf
+++ vault-int/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "http://65.0.183.77:8200"
+  address = "<>:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "d49015cf-8e3f-f023-9ef9-9353e801e537"
-      secret_id = "5cf76e46-13e7-aad8-86d2-42af9de5aaba"
+      role_id = "<>"
+      secret_id = "<>"
     }
   }
 }
@@ -26,7 +26,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
+    Name = "vault-init-terraform-sm"
     Secret = data.vault_kv_secret_v2.example.data["vault_integration"]
   }
 }
